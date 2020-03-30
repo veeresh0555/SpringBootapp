3f64c2b77dac98b4fcfb120bfecd80ea03d27961
veeru <veeru@DESKTOP-EPVTAH2>
2020-03-30 08:21:12 +0530

    Basic SpringBoot App

---
+++ employeActivities/src/main/java/com/example/EmployeActivitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeActivitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeActivitiesApplication.class, args);
+	}
+
+}
